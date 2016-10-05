43695dd70a0d6eeddee905d934a989c9c8e1efef
kaitsu1 <kairi.papstel@gmail.com>
2016-10-06 01:02:59 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kaits on 06.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+        System.out.println("Julge Pealehakkamine On Pool Voitu!");
+    }
+}
